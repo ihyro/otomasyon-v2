afae86b8216a89d938ca355d0e4a882384ca63b9
Kaan <57954502+ihyro@users.noreply.github.com>
2023-05-16 10:38:20 +0300

    v2

---
+++ Data/ImageListDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otomasyon.Data
+{
+    public class ImageListDTO
+    {
+        public string Url { get; set; }
+        public int Order { get; set; }
+    }
+}
